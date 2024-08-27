94aa9135dafc737976a6bcdf0c9b2ce16d56c28b
alex0d <alexey.d707@gmail.com>
2024-08-27 23:32:54 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,32 +21,32 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
-    implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.2")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.8")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("io.ktor:ktor-client-okhttp:2.3.11")
 
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    implementation("com.aallam.openai:openai-client:3.7.2")
-    implementation("ru.tinkoff.piapi:java-sdk-core:1.5")
+    implementation("com.aallam.openai:openai-client:3.8.2")
+    implementation("ru.tinkoff.piapi:java-sdk-core:1.22")
 
-    runtimeOnly("org.postgresql:postgresql:42.5.4")
+    runtimeOnly("org.postgresql:postgresql:42.7.3")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
-    testImplementation("org.springframework.security:spring-security-test:6.0.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.3")
+    testImplementation("org.springframework.security:spring-security-test:6.3.0")
 }
 
 tasks.withType<KotlinCompile> {
